e04fb9cfb71f728737661746c9d26856b999d82c
vangheem <vangheem@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-05-16 18:35:33 +0000

    removed folder for utils and just made it a file.

---
+++ Products/ImageEditor/utils.py
@@ -0,0 +1,37 @@
+import random
+
+def normalize_name(name):
+    return name.lower().replace(' ', '-').replace(':', "-")
+
+def generate_random_url(context):
+    return context.absolute_url() + "/currenteditedimage.jpg?" + str(random.randint(0, 1000000))
+    
+def get_image_information(editor):
+    image_info = editor.get_current_image_info()
+    return {
+        'url' : generate_random_url(editor.context),
+        'can_undo' : editor.unredo.can_undo(),
+        'can_redo' : editor.unredo.can_redo(),
+        'can_save' : editor.unredo.pos > 0,
+        'size' : image_info['sizeformatted'],
+        'width' : image_info['width'],
+        'height' : image_info['height']
+    }
+    
+def json(d):
+    
+    def convert_type(value):
+        if type(value) == bool:
+            return str(value).lower()
+        elif type(value) == str:
+            return "'%s'" % value
+        elif type(value) == dict:
+            return json(value)
+        elif type(value) == list or type(value) == tuple:
+            return "[%s]" % ','.join([convert_type(v) for v in value])
+        else:
+            return value
+        
+    return "{%s}" % (', '.join(["%s : %s" % (name, convert_type(value)) for name, value in d.items()]))
+    
+    
