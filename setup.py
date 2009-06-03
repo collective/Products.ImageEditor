6bc5cb6d413f26ce842e9f7d5889be9e359e808a
toutpt <toutpt@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-03 20:48:51 +0000

    add collective.js.jquery as dependencies

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(name='Products.ImageEditor',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'collective.js.jquery'
           # -*- Extra requirements: -*-
       ],
       entry_points="""
