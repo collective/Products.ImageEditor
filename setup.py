5ff8d616391c756e1adb8729f81958a574e8c7e0
vangheem <vangheem@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-12-06 09:26:30 +0000

    removed dependencies.txt file-that was a foolish way of doing it

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import setup, find_packages
 import os
 
-dependencies = [
-    'setuptools'
-]
-
-#now get plone dependencies
-dep_file = open(os.path.join('Products', 'ImageEditor', 'dependencies.txt'))
-dependencies.extend([ d.strip('\n').strip() for d in dep_file.readlines() ])
-
 setup(name='Products.ImageEditor',
       version='1.2',
-      description="A product that adds an editor tab to ATImage and News Item so you can rotate, flip, blur, compress, contrast, brightness, sharpen, add drop shadows, crop, resize an image.",
+      description="A product that adds an editor tab to ATImage and News Item "
+                  "so you can rotate, flip, blur, compress, contrast, brightness, "
+                  "sharpen, add drop shadows, crop, resize an image.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -20,7 +14,8 @@ setup(name='Products.ImageEditor',
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='atimage plone image rotate flip blur compress contrast brightness sharpen drop shadows crop resize',
+      keywords='plone image rotate flip blur compress contrast brightness '
+               'sharpen drop shadows crop resize save as sepia',
       author='Nathan Van Gheem',
       author_email='vangheem@gmail.com',
       url='http://www.plone.org/products/products-imageeditor',
@@ -29,7 +24,11 @@ setup(name='Products.ImageEditor',
       namespace_packages=['Products'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=dependencies,
+      install_requires=[
+        'setuptools',
+        'collective.js.jquery',
+        'collective.js.jqueryui'
+      ],
       entry_points="""
       # -*- Entry points: -*-
       """,
