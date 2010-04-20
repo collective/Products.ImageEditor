8651da7033f939afc8a265c59b12f4ffc3df3a8f
witsch <witsch@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-20 16:02:18 +0000

    hook up view to allow invocation of the editor directly from Archetypes' image widget

--- Products/ImageEditor/browser/resources/editlink.js
+++ Products/ImageEditor/browser/resources/editlink.js
@@ -1,12 +1,14 @@
 (function($){
 $(document).ready(function(){
 
+    $('body').append('<div id="image-editor" />');
     $('div > div.field:first-child > img:first-child').
         parent().parent().next().
         append('&mdash; <a href="">Edit this image</a>').
         click(function(event) {
             event.preventDefault();
-            alert('...edit...');
+            var url = window.location.pathname + '/../@@imageeditor.inline';
+            $('#image-editor').load(encodeURI(url));
         });
 
 });
