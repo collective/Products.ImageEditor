b931733a696c2f079818070d4ae0c996ccfe8183
vincentfretin <vincentfretin@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-02-24 11:05:28 +0000

    Keep "Current image" link behaviour untouched. Previously, clicking the "Current image" link opened the editor.

--- Products/ImageEditor/browser/resources/editlink.js
+++ Products/ImageEditor/browser/resources/editlink.js
@@ -4,10 +4,11 @@ $(document).ready(function(){
     $('body').append('<div id="image-editor" />');
     $('div > div.field:first-child > img:first-child').
         parent().parent().next().
-        append('&mdash; <a href="">Edit this image</a>').
+        append('&mdash; <a href="" id="edit-image">Edit this image</a>');
+        $('#edit-image').
         click(function(event) {
             event.preventDefault();
-            var field = $(this).parent()[0].id.replace('archetypes-fieldname-', '')
+            var field = $(this).parent()[0].id.replace('archetypes-fieldname-', '');
             var url = window.location.pathname + '/../@@imageeditor.inline?field=' + field;
             $('#image-editor').load(encodeURI(url), function() {
                 $('.ui-dialog-titlebar-close').attr('href',
