062ba9d95bdb5db90ff8a98928e3d1dc8a9843c5
thomasdesvenain <thomasdesvenain@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-22 11:25:01 +0000

    Do not remove jquery ui 'global_include' on install  not to break third part products using it.

--- Products/ImageEditor/upgrades.py
+++ Products/ImageEditor/upgrades.py
@@ -8,15 +8,15 @@ def remove_stack_pos_and_unredostack_from_images_in_catalog(catalog):
 
     for image in images:
         image = image.getObject()
-    
+
         if hasattr(image, 'stack_pos'):
             delattr(image, 'stack_pos')
-        
+
         if hasattr(image, 'unredostack'):
             delattr(image, 'unredostack')
-        
+
         image._p_changed = 1
-    
+
 def remove_image_editor_tab(portal):
     portal_actions = getToolByName(portal, 'portal_actions')
     object_buttons = portal_actions.object
@@ -36,48 +36,49 @@ def remove_visual_editor_tab(portal):
             if a.id == 'editor' and a.category == 'object':
                 action = a
                 break
-            
+
         actions.remove(action)
         portal_types.Image._actions = tuple(actions)
         portal_types.Image._p_changed = 1
-        
+
 
 def upgrade_to_1_2(context):
     portal = getToolByName(context, 'portal_url').getPortalObject()
-    
+
     catalog = getToolByName(portal, 'portal_catalog')
     remove_stack_pos_and_unredostack_from_images_in_catalog(catalog)
     remove_image_editor_tab(portal)
     remove_visual_editor_tab(portal)
-    
+
     context.runAllImportStepsFromProfile(to_1_2_profile)
     context.runAllImportStepsFromProfile(default_profile)
-    
+
 def upgrade_to_1_3(context):
     context.runImportStepFromProfile(default_profile, 'jsregistry')
-    
+
 ie_views = ['imageeditor', 'imageeditor.alagimp', 'imageeditor.inline', 'imageeditor.slider']
-def set_unintrusive_jqueryui_properties(portal):
+def set_unintrusive_jqueryui_properties(portal, global_include=None):
     pprops = getToolByName(portal, 'portal_properties')
     jq_props = pprops.jqueryui_properties
-    jq_props.global_include = False
+    if global_include is not None:
+        jq_props.global_include = global_include
+
     jq_props.views_and_templates = tuple(set(jq_props.getProperty('views_and_templates', [])) | set(ie_views))
-    
+
 def upgrade_to_1_7(context):
     portal = getToolByName(context, 'portal_url').getPortalObject()
     qi = getToolByName(portal, 'portal_quickinstaller')
-    
+
     if not qi.isProductInstalled('collective.js.jqueryui'):
         qi.installProduct('collective.js.jqueryui')
-        set_unintrusive_jqueryui_properties(portal)
+        set_unintrusive_jqueryui_properties(portal, global_include=False)
     else:
         pprops = getToolByName(portal, 'portal_properties')
         if 'jqueryui_properties' not in pprops.objectIds():
             # installed, but not upgraded
             from collective.js.jqueryui.upgrades import upgrade_1891_1892
             upgrade_1891_1892(context)
-            
-        
-    
-    
-    
+
+
+
+
