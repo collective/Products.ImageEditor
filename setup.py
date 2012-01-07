8919979e44c39df87b413812441ab03628132b02
hvelarde <hector.velarde@gmail.com>
2012-01-06 20:08:30 -0600

    renamed file as GitHub needs to know it is reStructuredText formated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os.path import join
 
-readme = open("README.txt").read()
+readme = open("README.rst").read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
 setup(name='Products.ImageEditor',
